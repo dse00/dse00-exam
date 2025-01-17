e9e76a47ff2c7f0c17cb1b1b98e1b0fca544dc6a
LIK WAI NG <nglikwai@ymail.com>
2025-01-17 20:46:36 +0000

    fix: leaderboard

--- src/hooks/useRanking.ts
+++ src/hooks/useRanking.ts
@@ -1,5 +1,4 @@
 import { useQuery, useQueryClient } from '@tanstack/react-query';
-import Cookies from 'js-cookie';
 
 import { QUERY_KEYS } from '@/constants';
 import services from '@/services';
@@ -7,15 +6,12 @@ import services from '@/services';
 export const useRanking = () => {
   const queryClient = useQueryClient();
 
-  const token = Cookies.get('token') as string;
-
   const { data: rankingData } = useQuery({
     queryKey: [QUERY_KEYS.RANKING],
     queryFn: () => {
       return services.getRanking();
     },
     refetchOnWindowFocus: false,
-    enabled: !!token,
     staleTime: 1000 * 60 * 60 * 24, // 1 day
   });
 
