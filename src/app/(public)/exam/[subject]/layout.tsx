1036c9b7f953f0a53ad0c4a75326a4055d26a3d9
LIK WAI NG <nglikwai@ymail.com>
2025-01-17 21:22:34 +0000

    fix: props

--- src/app/(public)/exam/[subject]/layout.tsx
+++ src/app/(public)/exam/[subject]/layout.tsx
@@ -1,13 +1,7 @@
 import { webData } from '@/constants';
 import services from '@/services';
 
-export async function generateMetadata({
-  params,
-  searchParams,
-}: {
-  params: Promise<{ subject: string }>;
-  searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
-}) {
+export async function generateMetadata({ params }: { params: Promise<{ subject: string }> }) {
   // Fetch dynamic data (e.g., from an API or database)
   const p = await params;
   const mapData = await services.getPaperNameMapping();
