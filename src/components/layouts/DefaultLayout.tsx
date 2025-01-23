519ecd4a0c5f1ee68575eda62e2ecfdf306230d9
LIK WAI NG <nglikwai@ymail.com>
2025-01-23 19:37:10 +0000

    fix: login

--- src/components/layouts/DefaultLayout.tsx
+++ src/components/layouts/DefaultLayout.tsx
@@ -1,8 +1,6 @@
 import Footer from '@/app/_components/Footer';
 import Header from '@/app/_components/Header';
 
-import LoginDialog from '../LoginDialog';
-
 export type LayoutProps = {
   children: React.ReactNode;
 };
@@ -10,7 +8,6 @@ export type LayoutProps = {
 export default ({ children }: LayoutProps) => {
   return (
     <div className='flex flex-col items-center sm:gap-10 gap-8'>
-      <LoginDialog />
       <Header />
       <div className='container justify-center px-2 min-h-[75vh]'>{children}</div>
       <Footer />
