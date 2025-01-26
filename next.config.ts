9dff698d84909f7faff329a4e523fb1121177635
LIK WAI NG <nglikwai@ymail.com>
2025-01-26 15:26:07 +0000

    fix: route

--- next.config.ts
+++ next.config.ts
@@ -2,20 +2,13 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  assetPrefix: '',
   images: {
     unoptimized: true,
     remotePatterns: [
       {
         protocol: 'https',
         hostname: 'asset.dse00.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'assets.leetcode.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'leetcode.com',
       },
     ],
   },
